4ef899b211ca74413d40203cf3febcf407ea0630
Dave Youn <dveyoun@gmail.com>
2025-07-27 12:06:20 -0700

    update requirements

--- llm_client.py
+++ llm_client.py
@@ -2,14 +2,12 @@ from langchain_openai import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
-def get_response():
+def get_response(user_input):
     llm = OpenAI(temperature=0.7) # Setting temperature for creativity
     prompt_template = PromptTemplate(
         input_variables=["user_input"],
         template="Create a poem based on lyrics from the Grateful Dead from the following: {user_input}",
     )
-    user_input = "going to san francisco"
-
     chain = LLMChain(llm=llm, prompt=prompt_template)
 
     response = chain.run(user_input)
@@ -17,5 +15,6 @@ def get_response():
     return response
 
 if __name__ == "__main__":
-    response = get_response()
-    print("Creative Product Idea:", response)
+    user_input = 'going to san francisco'
+    response = get_response(user_input)
+    print(f"A Grateful Dead Poem based on '{user_input}'", response)
